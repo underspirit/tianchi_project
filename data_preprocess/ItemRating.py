e64801fa5c5f9dcc846de1c3a62b50c9d04abb49
Jayvee_He <jayveehe@gmail.com>
2015-04-09 00:12:33 +0800

    修改用户对item评分的函数

--- data_preprocess/ItemRating.py
+++ data_preprocess/ItemRating.py
@@ -27,20 +27,24 @@ dataobj = json.loads(jsonstr)
 def rate_items():
     A1 = 1.1
     A2 = 2
-    A3 = 3
+    A3 = 5
     mongodb_utils = MongodbUtils('10.108.192.165', 27017)
     db = mongodb_utils.get_db()
     train_user = db.train_user
-    root = {}
-    logger.info('start!')
-    usercount = 0.0
+    root = []
+    logger.info('loading...')
+    usercount = 0
     ids = train_user.distinct("user_id")
     maxusercount = len(ids)
+    logger.info('start!')
+    split_json = open('../data/split_json_item_rates.json', 'w')
     for user_id in ids:
+        user_rates_info = {'user_id': user_id}
         user_rates = {}
         itemcount = 0
         usercount += 1
-        if usercount.__mod__(0.01 * maxusercount) == 0:
+        print usercount
+        if usercount.__mod__(0.01 * maxusercount) == 0.0:
             logger.debug(str(usercount / (0.01 * maxusercount)) + r'% done!')
         for item_id in train_user.find({"user_id": user_id}).distinct("item_id"):
             itemcount += 1
@@ -49,19 +53,22 @@ def rate_items():
             count3 = 0
             for behavior in train_user.find({"user_id": user_id, "item_id": item_id}):
                 behavior_type = behavior['behavior_type']
-                if behavior_type is '1':
+                if behavior_type == '1':
                     count1 += 1
-                elif behavior_type is '2' or '3':
+                elif behavior_type == '2' or behavior_type == '3':
                     count2 += 1
-                elif behavior_type is '4':
+                elif behavior_type == '4':
                     count3 += 1
             rate = A1 ** count1 + A2 ** count2 + A3 ** count3
             user_rates[item_id] = rate
-        user_rates['item_count'] = itemcount
-        root[user_id] = user_rates
+        user_rates_info['item_count'] = itemcount
+        user_rates_info['items'] = user_rates
+        tempstr = json.dumps(user_rates_info)
+        split_json.write(tempstr+'\n')
+        root.append(user_rates_info)
     return root
 
 
 if __name__ == "__main__":
     root = rate_items()
-    json.dump(root, open('../data/user_item_data.json', 'w'))
+    json.dump(root, open('../data/user_item_rates_new.json', 'w'))
